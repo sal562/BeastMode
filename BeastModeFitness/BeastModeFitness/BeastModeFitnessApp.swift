830ce80fd0cc25e2306c312a22b2e6086892953c
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-07-07 14:16:18 +0700

    setup project

---
+++ BeastModeFitness/BeastModeFitness/BeastModeFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeastModeFitnessApp.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BeastModeFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeastModeFitness/BeastModeFitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
