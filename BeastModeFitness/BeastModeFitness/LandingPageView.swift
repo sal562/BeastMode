c6c9c765c4979cc546993874adaa7778a04fd871
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-07-09 11:51:46 +0700

    added geometryReader to center

--- BeastModeFitness/BeastModeFitness/LandingPageView.swift
+++ BeastModeFitness/BeastModeFitness/LandingPageView.swift
@@ -26,52 +26,53 @@ struct LandingPageView: View {
     
     var body: some View {
                    
-        NavigationView {
-            VStack {
-                    Spacer()
-                    Text("BEAST MODE")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .animation(.easeInOut)
-                        .scaleEffect(scaledAmount ? 0.25 : 1.0)
-                    
-                    Spacer()
-                    
-                NavigationLink(
-                    destination: WorkoutPlanView(),
-                    tag: 1,
-                    selection: $selection) {
-                    Button(action: {
-                                //start a workout
-                                selection = 1
-                            }, label: {
-                                
-                                HStack(spacing: 20) {
-                                Image(systemName: "plus.square")
-                                    .font(.title)
-                                Text("Start A Workout")
-                                }//end of hstack
-                                .frame(width: 300, height: 60)
-                                .foregroundColor(.white)
-                                .background(Color.red)
-                                .shadow(radius: 10)
-                            })
-                            //end of button
-                    .cornerRadius(12.0)
-                }
-                
-
-                }
-                .background(
-                    Image("beastmodeBackground")
-                     .resizable()
-                     .scaledToFill()
-                     .edgesIgnoringSafeArea(.all)
-                )
-                .onAppear(perform: {
-                    titleIsAnimating.toggle()
-            })
+        GeometryReader { geometry in
+            NavigationView {
+                VStack {
+                        Spacer()
+                        Text("BEAST MODE")
+                            .font(.system(size: 48, weight: .heavy, design: .rounded))
+                            .foregroundColor(.white)
+                            .animation(.easeInOut)
+                            .scaleEffect(scaledAmount ? 0.25 : 1.0)
+                            .offset(y: -250.0)
+                        
+                        Spacer()
+                        
+                    NavigationLink(
+                        destination: WorkoutPlanView(),
+                        tag: 1,
+                        selection: $selection) {
+                        Button(action: {
+                                    //start a workout
+                                    selection = 1
+                                }, label: {
+                                    
+                                    HStack(spacing: 20) {
+                                    Image(systemName: "plus.square")
+                                        .font(.title)
+                                    Text("Start A Workout")
+                                    }//end of hstack
+                                    .frame(width: 300, height: 60)
+                                    .foregroundColor(.white)
+                                    .background(Color.red)
+                                    .shadow(radius: 10)
+                                })
+                                //end of button
+                        .cornerRadius(12.0)
+                    }
+                    } // Root VStack
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(
+                        Image("beastmodeBackground")
+                         .resizable()
+                         .scaledToFill()
+                         .edgesIgnoringSafeArea(.all)
+                    )
+                    .onAppear(perform: {
+                        titleIsAnimating.toggle()
+                })
+            }
         }
         }
 }
