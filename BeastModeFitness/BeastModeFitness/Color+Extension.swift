49aa6d84921b8710f7674e67e3ace81da09ad19c
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-07-09 15:59:08 +0700

    create Color+Extension.swift for custom backgroundColor

---
+++ BeastModeFitness/BeastModeFitness/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extension.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 7/9/21.
+//  Copyright © 2021 Sal B. Amer. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkPrimaryColor = Color(red: 110/255, green: 155/255, blue: 195/255, opacity: 0.3)
+}
