35ab4feab48c0fd76d1fa161851ec60c2bd1f378
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-07-09 16:04:38 +0700

    finished MainButtonCustomStyle

--- BeastModeFitness/BeastModeFitness/MainButton.swift
+++ BeastModeFitness/BeastModeFitness/MainButton.swift
@@ -10,14 +10,10 @@ import SwiftUI
 
 struct MainButtonCustomStyle: ButtonStyle {
     
-    var fillColor: Color = .darkPrimaryButton
-    
+    var fillColor: Color = .darkPrimaryColor
     func makeBody(configuration: Configuration) -> some View {
-        return MainButton(configuration: configuration)
+        return MainButton(configuration: configuration, fillColor: fillColor)
     }
-    
-    
-    
     struct MainButton: View {
         let configuration: Configuration
         let fillColor: Color
@@ -25,9 +21,18 @@ struct MainButtonCustomStyle: ButtonStyle {
         var body: some View {
             return configuration.label
                 .padding(20)
-                .background(RoundedRectangle(cornerRadius: 8))
-                .fillColor(fillColor)
+                .background(RoundedRectangle(cornerRadius: 8).fill(fillColor))
+                
         }
     }
 }
 
+struct MainButtonCustomStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Button(action: {
+            //button action
+        }, label: {
+            Text("Add a Workout")
+        }).buttonStyle(MainButtonCustomStyle())
+    }
+}
