68cf74f4e0c8d2fd28238c8d4f90bbf65e80b8a8
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-07-25 17:23:32 +0700

    create challenge in FB DB

--- BeastModeFitness/BeastModeFitness/NetworkServices/ChallengeService.swift
+++ BeastModeFitness/BeastModeFitness/NetworkServices/ChallengeService.swift
@@ -18,16 +18,18 @@ protocol ChallengeServiceProtocol {
     
     final class ChallengeService: ChallengeServiceProtocol {
         
-        //initialize firestore DB
+        ///initialize firestore DB
         private let db = Firestore.firestore()
         
+        ///create challenge in FB DB
         func create(_ challenge: Challenge) -> AnyPublisher<Void, Error> {
             return Future<Void, Error> { promise in
                 do {
-                    
+                    _ = try self.db.collection("challenges").addDocument(from: challenge)
+                    promise(.success(()))
                 } catch {
-                    
+                    promise(.failure(error))
                 }
-            }
+            }.eraseToAnyPublisher()
         }
     }
