81fa7bb0a4f955f385f690b52c8595f207cf92ba
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-08 19:22:48 +0700

    create ChallengeListView

---
+++ BeastModeFitness/BeastModeFitness/Views/ChallengeListView.swift
@@ -0,0 +1,21 @@
+//
+//  ChallengeListView.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 8/8/21.
+//  Copyright © 2021 Sal B. Amer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChallengeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChallengeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeListView()
+    }
+}
