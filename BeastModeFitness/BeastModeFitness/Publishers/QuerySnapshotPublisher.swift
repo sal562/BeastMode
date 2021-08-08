a85943a5914a7d6eb0537bf95ffb04064e59387f
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-08 19:26:58 +0700

    create QuerySnapshotPublisher

---
+++ BeastModeFitness/BeastModeFitness/Publishers/QuerySnapshotPublisher.swift
@@ -0,0 +1,18 @@
+//
+//  QuerySnapshotPublisher.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 8/8/21.
+//  Copyright © 2021 Sal B. Amer. All rights reserved.
+//
+
+import Foundation
+
+extension Publishers {
+    
+    struct QuerySnapshotPublisher {
+        
+        
+        
+    }
+}
