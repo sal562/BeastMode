785a710685c3e1d930d87a2edb60b8e2d665c771
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-17 17:17:23 +0700

    appStorage for shortcut for userDefault

--- BeastModeFitness/BeastModeFitness/ViewModels/SettingsViewModel.swift
+++ BeastModeFitness/BeastModeFitness/ViewModels/SettingsViewModel.swift
@@ -6,17 +6,30 @@
 //  Copyright © 2021 Sal B. Amer. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
 import Combine
 
 
 final class SettingsViewModel : ObservableObject {
 
     @Published private(set) var itemViewModels: [SettingsItemViewModel] = []
+    ///shortcut for userDefault
+    @AppStorage("isDarkMode") private var isDarkMode = false
     
+    ///new way using item(at) function to pass position
     func item(at index: Int) -> SettingsItemViewModel {
         ///grab index from itemViewModel
         itemViewModels[index]
+    }
+    ///new method of detecting tap
+    func tappedItem(at index: Int)   {
+        switch itemViewModels[index].type {
+        case .mode:
+            ///change from light to dark
+            isDarkMode = !isDarkMode
+        default:
+            break
+        }
     }
     
     ///func to trigger onApper and init default settings
--- BeastModeFitness/BeastModeFitness/Views/SettingsView.swift
+++ BeastModeFitness/BeastModeFitness/Views/SettingsView.swift
@@ -15,13 +15,19 @@ struct SettingsView: View {
     var body: some View {
         
         List(settingsVM.itemViewModels.indices, id:\.self) { index in
-            HStack {
-//                Image(systemName:settingsVM.itemViewModels[index].iconName)
-//                Text(settingsVM.itemViewModels[index].title)
-                ///new way using item(at) function
-                Image(systemName:settingsVM.item(at: index).iconName)
-                Text(settingsVM.item(at: index).title)
-            }
+            
+            ///Create Button to Detect if clicked
+            Button(action: {
+                settingsVM.tapped
+            }, label: {
+                HStack {
+    //                Image(systemName:settingsVM.itemViewModels[index].iconName)
+    //                Text(settingsVM.itemViewModels[index].title)
+                    ///new way using item(at) function
+                    Image(systemName:settingsVM.item(at: index).iconName)
+                    Text(settingsVM.item(at: index).title)
+                }
+            })
         }.onAppear(perform: {
             settingsVM.onAppear()
         })
