37c1424ca0ae92884246c3c900d434ccbfaf9569
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-18 12:41:32 +0700

    create LandingpageViewModel

---
+++ BeastModeFitness/BeastModeFitness/ViewModels/LandingpageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LandingpageViewModel.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 8/18/21.
+//  Copyright © 2021 Sal B. Amer. All rights reserved.
+//
+
+import Foundation
+
+final class LandingpageViewModel: ObservableObject {
+    
+    @Published var loginSignupPagePushed = false
+    @Published var createAccountPagePushed = false
+}
