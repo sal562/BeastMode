e99df4b98dbe1b2678b75ac7aa243131951e0b1a
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-18 15:06:16 +0700

    creat LoginSignupViewModel

---
+++ BeastModeFitness/BeastModeFitness/ViewModels/LoginSignupViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  LoginSignupViewModel.swift
+//  BeastModeFitness
+//
+//  Created by sal562 on 8/18/21.
+//  Copyright © 2021 Sal B. Amer. All rights reserved.
+//
+
+import Foundation
+
+final class LoginSignupViewModel: ObservableObject {
+    
+    
+}
+
+
+extension LoginSignupViewModel {
+    enum Mode {
+        case login
+        case signup
+    }
+}
