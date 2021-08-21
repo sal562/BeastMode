c6054a3eb7671da610112bfaddf42b511aa7c8e4
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-21 16:15:35 +0700

    Hard Coded - use constants for colors

--- BeastModeFitness/BeastModeFitness/Color+Extension.swift
+++ BeastModeFitness/BeastModeFitness/Color+Extension.swift
@@ -12,6 +12,8 @@ import SwiftUI
 
 extension Color {
     static let darkPrimaryColor = Color(red: 110/255, green: 155/255, blue: 195/255, opacity: 0.3)
-    
     static let primaryButton = Color("primaryButton")
+    static let circleOutline = Color("CircleOutline")
+    static let circleTrack = Color("CircleTrack")
+    
 }
--- BeastModeFitness/BeastModeFitness/Views/ProgressCircleView.swift
+++ BeastModeFitness/BeastModeFitness/Views/ProgressCircleView.swift
@@ -10,6 +10,15 @@ import SwiftUI
 
 ///REQUIRES 2 custom colors for light and dark mode + outline CircleOutline,CircleTrack
 
+///save custom color as extension
+//extension Color {
+//    static let darkPrimaryColor = Color(red: 110/255, green: 155/255, blue: 195/255, opacity: 0.3)
+//    static let primaryButton = Color("primaryButton")
+//    static let circleOutline = Color("CircleOutline")
+//    static let circleTrack = Color("CircleTrack")
+//
+//}
+
 struct ProgressCircleViewModel {
     let title: String
     let message: String
@@ -31,7 +40,8 @@ struct ProgressCircleView: View {
             Circle()
                 .stroke(style: .init(lineWidth: 10, lineCap: .round, lineJoin: .round))
                 ///use customColor for circle outline
-                .fill(Color("CircleOutline"))
+//                .fill(Color("CircleOutline"))///Hard Coded - use constants
+                .fill(Color.circleOutline)
         }
     }
 }
@@ -39,5 +49,6 @@ struct ProgressCircleView: View {
 struct ProgressCircleView_Previews: PreviewProvider {
     static var previews: some View {
         ProgressCircleView(progressCircleVM: .init(title: "Day", message: "3 of 7", percentageComplete: 0.43))
+//            .preferredColorScheme(.dark) //FOR TESTING DARK MODE
     }
 }
