a083112e35ffba1919274b738343b06f48dc0e4f
Sal B. Amer <57122576+sal562@users.noreply.github.com>
2021-08-22 15:26:50 +0700

    created separate challengeCell

--- BeastModeFitness/BeastModeFitness/Views/ChallengeItemView.swift
+++ BeastModeFitness/BeastModeFitness/Views/ChallengeItemView.swift
@@ -69,7 +69,10 @@ struct ChallengeItemView: View {
 
 
 struct ChallengeItemView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ChallengeItemView(viewModel: ChallengeItemViewModel.)
+        ChallengeItemView(viewModel:  ChallengeItemViewModel(Challenge(userId: "12121", startDate: Date(), exercise: "Pullups", startAmount: 2, increase: 1, length: 14)))
+            .frame(width: 300, height: 300)
+            .preferredColorScheme(.dark) //FOR TESTING DARK MODE
     }
 }
